d83536976f24bee92651be9abc346c9ef9472d0b
sturfee-petrl <peterl@sturfee.com>
2017-08-25 18:26:02 -0700

    revert to sync.Map

--- kmutex.go
+++ kmutex.go
@@ -3,36 +3,29 @@ package kmutex
 import "sync"
 
 type Kmutex struct {
-	mut *sync.Mutex
-	m   map[interface{}]*sync.Mutex
+	m sync.Map
 }
 
 func NewKmutex() Kmutex {
-	m := sync.Mutex{}
-	return Kmutex{&m, map[interface{}]*sync.Mutex{}}
+	return Kmutex{sync.Map{}}
 }
 
-func (s Kmutex) Unlock(key interface{}) {
-	s.mut.Lock()
-	defer s.mut.Unlock()
-	m := s.m[key]
-	delete(s.m, key)
-	m.Unlock()
+func (s Kmutex) Unlock(key interface{})  {
+	l, _ := s.m.Load(key)
+	l_ := l.(*sync.Mutex)
+	s.m.Delete(key)
+	l_.Unlock()
 }
 
 func (s Kmutex) Lock(key interface{}) {
 	m := sync.Mutex{}
-	s.mut.Lock()
-	m_, busy := s.m[key]
-	if busy {
-		s.mut.Unlock()
-		m_.Lock()
-		m_.Unlock()
+	m_, _ := s.m.LoadOrStore(key, &m)
+	mm := m_.(*sync.Mutex)
+	mm.Lock()
+	if mm != &m {
+		mm.Unlock()
 		s.Lock(key)
 		return
 	}
-	m.Lock()
-	s.m[key] = &m
-	s.mut.Unlock()
 	return
 }
