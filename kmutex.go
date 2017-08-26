8a47cbd425713f7ee78fb75763cec4c332753c0c
sturfee-petrl <peterl@sturfee.com>
2017-08-25 18:16:33 -0700

    first version

---
+++ kmutex.go
@@ -0,0 +1,34 @@
+package kmutex
+
+import "sync"
+
+type storage struct {
+	m sync.Map
+}
+
+type Locker struct {
+	key interface{}
+	l   *sync.Mutex
+	s   *sync.Map
+}
+
+func NewStorage() storage {
+	return storage{sync.Map{}}
+}
+
+func (l Locker) Unlock() {
+	l.s.Delete(l.key)
+	l.l.Unlock()
+}
+
+func (s storage) Acquire(key interface{}) Locker {
+	m := sync.Mutex{}
+	m_, _ := s.m.LoadOrStore(key, &m)
+	mm := m_.(*sync.Mutex)
+	mm.Lock()
+	if mm != &m {
+		mm.Unlock()
+		return s.Acquire(key)
+	}
+	return Locker{l: &m, s: &s.m, key: key}
+}
